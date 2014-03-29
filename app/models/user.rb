a73a0a2c62924a30318c4518deef1617c838ef71
shouponlin <shoupon.lin@gmail.com>
2014-03-29 00:49:29 -0400

    Preliminary facebook login route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User
   include Mongoid::Document
   field :email, type: String
+  field :provider, type: String
+  field :uid, type: String
+  field :name, type: String
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+    end
+  end
 end
