27a207844fcddd9a076930fa02c4e765dbcbba91
shouponlin <shoupon.lin@gmail.com>
2014-03-28 23:05:08 -0400

    Initialize Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlatoForum::Application.config.secret_key_base = 'e1ed31bc5512ffb47d32959ca7a2707f1adea48176a0ccda7c028babf961cef5ef36a89e7e9d297dd9af2a015969678a3b3f5e64382989e290313ffbab2ee8e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PlatoForum::Application.config.session_store :cookie_store, key: '_PlatoForum_session'
