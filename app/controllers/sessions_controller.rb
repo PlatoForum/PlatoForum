177305bb01aec5f7f6ab0370912ed3243f8593f7
shouponlin <shoupon.lin@gmail.com>
2014-03-29 14:35:18 -0400

    Sign in/out using facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,14 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.where(:provider => auth['provider'],
+                      :uid => auth['uid']).first || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
+    redirect_to user_path(user), :notice => "Signed in!"
   end
 
-def destroy
-  session[:user_id] = nil
-  redirect_to root_url, :notice => "Signed out!"
-end
+  def destroy
+    reset_session
+    redirect_to root_url
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,9 @@ class User
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.name = auth["info"]["name"]
+      if auth['info']
+        user.name = auth['info']['name'] || ""
+      end
     end
   end
 end
