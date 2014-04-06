3c0bfadf317a940981b0524d8391dc30311ffaff
Hsuan Lee <boczeratul@gmail.com>
2014-04-05 22:56:57 -0400

    Improve layout

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -17,6 +17,7 @@ class Topic
   validates_presence_of :permalink
   validates_uniqueness_of :permalink
   validates_format_of :permalink, :with => /\A\w+\z/
+  validates_length_of :name, :maximum => 10
 
   validates_presence_of :doc
 end
