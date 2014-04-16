f8fc6027ed138fc704d29a30954ddb838ff88159
Hsuan Lee <BOCZeratul@gmail.com>
2014-04-15 21:29:41 -0600

    Add encoding specifications

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class AdminsController < ApplicationController
 	before_filter :check_level, :except => [:de_robot]
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
