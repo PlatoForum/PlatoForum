8cf2e0a5e4a792751fe7de84d1dcc103afdd71dc
Hsuan Lee <BOCZeratul@gmail.com>
2014-04-15 21:23:36 -0600

    Change config

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+ENV['GEM_PATH'] = '$HOME/ruby/gems:/usr/lib64/ruby/gems/1.9.3'
 
 # Initialize the Rails application.
 PlatoForum::Application.initialize!
