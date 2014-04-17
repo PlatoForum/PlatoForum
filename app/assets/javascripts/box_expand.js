ba145687a54fedc28a30359aab3a18eef5becb57
Hsuan Lee <boczeratul@gmail.com>
2014-04-17 12:23:21 -0400

    Improve layout

---
+++ app/assets/javascripts/box_expand.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('.expand-btn').click(function(e) {
+    var expandable = $(this).parent().parent()
+    if(expandable.hasClass('open')) {
+      $('.expandable-box.out').not(expandable).removeClass('out');
+      expandable.removeClass('open');
+    }
+    else {
+      $('.expandable-box').not(expandable).addClass('out');
+      expandable.addClass('open');
+    }
+  });
+});
