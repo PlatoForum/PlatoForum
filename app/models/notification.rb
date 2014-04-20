d8d08158e9eb63c2e8c6237dda3b4607dabd3a8c
Hsuan Lee <boczeratul@gmail.com>
2014-04-20 01:48:31 -0400

    Improve layout

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -15,11 +15,39 @@ class Notification
 
   belongs_to :target, class_name: "User", inverse_of: :notifications, autosave: true
 
-  before_save :push_notification
+  #before_save :push_notification
 
   def push_notification
     if self.target.level == 10
-      GRAPH_API.put_connections(self.target.uid, "notifications", template: "foo", href: "bar")
+      GRAPH_API.put_connections(self.target.uid, "notifications", template: self.display_message, href: "/notification/#{self.id}")
+    end
+  end
+
+  def display_message
+    case self.noti_type
+      when :NewComment
+        comment = Comment.find(self.source_id) 
+        return "#{comment.owner.display_name} 在 #{comment.topic.name} 中發佈了一則新評論 #{comment.display_abstract}" 
+      when :NewSupport
+        comment = Comment.find(self.source_id) 
+        target = Comment.find(self.destination_id) 
+        return "#{comment.owner.display_name} 支援了你在 #{comment.topic.name} 上的評論 #{target.display_abstract}" 
+      when :NewOppose 
+        comment = Comment.find(self.source_id) 
+        target = Comment.find(self.destination_id) 
+        return "#{comment.owner.display_name} 反駁了你在 #{comment.topic.name} 上的評論 #{target.display_abstract}" 
+      when :NewLike 
+        someone = Proxy.find_by(:id => self.source_id) 
+        target = Comment.find(self.destination_id) 
+        return "#{someone.display_name} 覺得你在 #{target.topic.name} 上的評論 #{target.display_abstract} 很讚！" 
+      when :NewDislike 
+        someone = Proxy.find_by(:id => self.source_id) 
+        target = Comment.find(self.destination_id) 
+        return "#{someone.display_name} 覺得你在 #{target.topic.name} 上的評論 #{target.display_abstract} 很爛！"
+      when :Other
+        return self.source_id
+      when :Announcement
+        return self.source_id
     end
   end
 end
