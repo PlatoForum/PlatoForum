2db82574bae6a87853b834f72f190aa949b8bff7
Hsuan Lee <boczeratul@gmail.com>
2014-04-19 22:36:09 -0400

    Improve layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
   #before_filter :check_first_visit
   before_filter :check_user
+  after_filter :allow_iframe
 
   helper_method :check_user, :current_user, :current_proxy, :pseudonym_gen
 
@@ -28,6 +29,10 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def allow_iframe
+    response.headers["X-Frame-Options"] = "GOFORIT"
+  end
 
   def not_found
     raise ActionController::RoutingError.new('Not Found')
