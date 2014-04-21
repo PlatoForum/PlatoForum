4c230535f509460de4721359d85ad3106b0e788b
Hsuan Lee <boczeratul@gmail.com>
2014-04-21 18:50:40 -0400

    Improve layout

--- app/assets/javascripts/topic_view/topic_view.js
+++ app/assets/javascripts/topic_view/topic_view.js
@@ -15,50 +15,6 @@ $(function() {
       $('.stance-back').show();
     }
   });
-
-  var data = [
-    <% @topic.stances.each do |stance| %>
-    {
-      value: <%= stance.comments.count %>,
-      color: "<%= stance.color_code %>",
-      label : "<%= stance.description %>",
-      labelColor : "<%= stance.color_inverse_code %>",
-      labelFontSize : "14px"
-    },
-    <% end %>
-  ]
-
-  var options = {
-    //Boolean - Whether we should show a stroke on each segment
-    segmentShowStroke : true,
-    
-    //String - The colour of each segment stroke
-    segmentStrokeColor : "#fff",
-    
-    //Number - The width of each segment stroke
-    segmentStrokeWidth : 2,
-    
-    //Boolean - Whether we should animate the chart 
-    animation : true,
-    
-    //Number - Amount of animation steps
-    animationSteps : 100,
-    
-    //String - Animation easing effect
-    animationEasing : "easeOutBounce",
-    
-    //Boolean - Whether we animate the rotation of the Pie
-    animateRotate : true,
-
-    //Boolean - Whether we animate scaling the Pie from the centre
-    animateScale : false,
-    
-    //Function - Will fire on animation completion.
-    onAnimationComplete : null
-  }
-  var ctx = document.getElementById("topic_statistics").getContext("2d");
-  var myNewChart = new Chart(ctx).Doughnut(data,options);
-
 });
 
 $(document).keydown(function (e) {
