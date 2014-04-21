f0544b0d4aedb7ddfc251e38584a19bd4f0d3244
Hsuan Lee <boczeratul@gmail.com>
2014-04-21 10:38:24 -0400

    Improve layout

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -19,7 +19,7 @@ class Notification
   after_create :push_notification
 
   def push_notification
-    if self.target.noti_settings and self.target.noti_settings[self.noti_type.to_s]
+    if (self.target.noti_settings and self.target.noti_settings[self.noti_type.to_s]) or noti_type == :Announcement
       #logger.error "Pushed!"
       GRAPH_API.put_connections(self.target.uid, "notifications", template: self.display_message, href: "notification/#{self.id}")
     end
