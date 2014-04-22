e8fdc7f195072d78e7d62d7d77312aacb4d98d02
Hsuan Lee <boczeratul@gmail.com>
2014-04-22 13:42:28 -0400

    Improve layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,45 +9,45 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def notification_message(notification)
-    if notification.noti_type == :NewComment 
-      comment = Comment.find(notification.source_id) 
+  def notification_message(noti)
+    if noti.noti_type == :NewComment 
+      comment = Comment.find(noti.source_id) 
       "#{comment.owner.display_name} 在 #{comment.topic.name} 中發佈了一則新評論 #{comment.display_abstract}" 
-    elsif notification.noti_type == :NewSupport 
-      comment = Comment.find(notification.source_id) 
-      target = Comment.find(notification.destination_id) 
+    elsif noti.noti_type == :NewSupport 
+      comment = Comment.find(noti.source_id) 
+      target = Comment.find(noti.destination_id) 
      "#{comment.owner.display_name} 支援了你在 #{comment.topic.name} 上的評論 #{target.display_abstract}" 
-    elsif notification.noti_type == :NewOppose 
-      comment = Comment.find(notification.source_id) 
-      target = Comment.find(notification.destination_id) 
+    elsif noti.noti_type == :NewOppose 
+      comment = Comment.find(noti.source_id) 
+      target = Comment.find(noti.destination_id) 
       "#{comment.owner.display_name} 反駁了你在 #{comment.topic.name} 上的評論 #{target.display_abstract}" 
-    elsif notification.noti_type == :NewLike 
-      someone = Proxy.find_by(:id => notification.source_id) 
-      target = Comment.find(notification.destination_id) 
+    elsif noti.noti_type == :NewLike 
+      someone = Proxy.find_by(:id => noti.source_id) 
+      target = Comment.find(noti.destination_id) 
       "#{someone.display_name} 覺得你在 #{target.topic.name} 上的評論 #{target.display_abstract} 很讚！" 
-    elsif notification.noti_type == :NewDislike 
-      someone = Proxy.find_by(:id => notification.source_id) 
-      target = Comment.find(notification.destination_id) 
+    elsif noti.noti_type == :NewDislike 
+      someone = Proxy.find_by(:id => noti.source_id) 
+      target = Comment.find(noti.destination_id) 
       "#{someone.display_name} 覺得你在 #{target.topic.name} 上的評論 #{target.display_abstract} 很爛！"
-    elsif notification.noti_type == :Other
-      notification.source_id
-    elsif notification.noti_type == :Announcement
-      notification.source_id
+    elsif noti.noti_type == :Other
+      noti.source_id
+    elsif noti.noti_type == :Announcement
+      noti.source_id
     end
   end
 
-  def notification_url(notification)
-    comment = Comment.find(notification.source_id)  unless notification.source_id.nil?
-    target = Comment.find(notification.destination_id) unless notification.destination_id.nil?
+  def notification_url(noti)
+    comment = Comment.find(noti.source_id)  unless noti.source_id.nil?
+    target = Comment.find(noti.destination_id) unless noti.destination_id.nil?
 
-    case notification.noti_type 
+    case noti.noti_type 
           when :NewComment then "/#{comment.topic.permalink}/comment_#{comment.id}"  
           when :NewSupport then "/#{comment.topic.permalink}/comment_#{comment.id}"  
           when :NewOppose then "/#{comment.topic.permalink}/comment_#{comment.id}"  
           when :NewLike then "/#{target.topic.permalink}/comment_#{target.id}"
           when :NewDislike then "/#{target.topic.permalink}/comment_#{target.id}"
-          when :Other then notification.destination_id
-          when :Announcement then notification.destination_id
+          when :Other then noti.destination_id
+          when :Announcement then noti.destination_id
       end
   end
 
