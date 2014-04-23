fbfb16ab01cce956e1b01567bf2371b20ebd9383
Hsuan Lee <boczeratul@gmail.com>
2014-04-23 00:06:43 -0400

    Fix youtube and create 'activate_media' function

--- app/assets/javascripts/topic_view/topic_view.js
+++ app/assets/javascripts/topic_view/topic_view.js
@@ -1,5 +1,25 @@
 /* Hsuan Lee @ 2014.04.20 */
 
+var imgStr = "\.(jpg|gif|png)+"
+var youtubeStr = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+
+// Expand <a /> with its associate application, ex. img, youtube
+function activate_media(){
+  $("#main-container .comment_bodies a").filter(function(){
+      // image url
+    var url = $(this).attr("href");
+    if( url.match(imgStr)) {
+      $(this).html($("<img>").attr("src",url).addClass("comment_img"));
+    }
+    // youtube url
+    var yt_match = url.match(youtubeStr);  // dessemble youtube url into fields
+    if( yt_match && yt_match[7].length==11) {
+
+      $(this).replaceWith("<div class='youtube-iframe'><iframe src='//www.youtube.com/embed/" + yt_match[7] + "' frameborder='0' allowfullscreen></iframe></div>");
+    }
+  });
+}
+
 function scroll_top () {
   var aTag = $("div[name='main-well']");
   $('html,body').animate({scrollTop: aTag.offset().top - 70},'slow');
@@ -76,6 +96,7 @@ function click_tr(data) {
     }
     else {
       $("#main-container").html($("#comment_"+data+"_full").html());
+      activate_media(data);
     }        
 
     scroll_top();
@@ -97,6 +118,7 @@ function click_comment_link(data) {
   }
   else {
     $("#main-container").html($("#comment_"+data+"_full").html());
+    activate_media(data);
   }        
 
   scroll_top();
